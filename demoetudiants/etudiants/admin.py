a510c1d02a3ae436c86ead123fa96e6639b4e525
papaye1855 <aboubacar1855@gmail.com>
2024-06-19 12:54:21 -0400

    Creation du model Etudiants

--- demoetudiants/etudiants/admin.py
+++ demoetudiants/etudiants/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Etudiants
 # Register your models here.
+
+admin.site.register(Etudiants)
--- demoetudiants/etudiants/models.py
+++ demoetudiants/etudiants/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Etudiants(models.Model):
+    nom = models.CharField(max_length=255)
+    prenom = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
